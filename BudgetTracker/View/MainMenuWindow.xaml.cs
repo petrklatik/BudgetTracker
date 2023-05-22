847d2525e4a42293698615205a51eacc07b99794
petrklatik <petrklatik@seznam.cz>
2023-05-22 22:36:10 +0200

    Adding some Viewmodel logic for the logging window

--- BudgetTracker/View/MainMenuWindow.xaml.cs
+++ BudgetTracker/View/MainMenuWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using BudgetTracker.ViewModel;
 using MahApps.Metro.Controls;
 
 namespace BudgetTracker.View
@@ -23,6 +24,7 @@ namespace BudgetTracker.View
         public MainMenuWindow()
         {
             InitializeComponent();
+            DataContext = new MainMenuViewModel();
         }
     }
 }
---
+++ BudgetTracker/ViewModel/MainMenuViewModel.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Input;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Windows;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace BudgetTracker.ViewModel
+{
+    public partial class MainMenuViewModel : ObservableValidator
+    {
+        [ObservableProperty]
+        private Visibility isLoggingFormVisibility;
+        [ObservableProperty]
+        private Visibility isProfileCreationFormVisibility;
+        [ObservableProperty]
+        private Visibility createProfileButtonVisibility;
+        [ObservableProperty]
+        private Visibility loginFormButtonVisibility;
+
+        [ObservableProperty]
+        [NotifyDataErrorInfo]
+        [Required]
+        [MinLength(1)]
+        private string username;
+
+        [ObservableProperty]
+        [NotifyDataErrorInfo]
+        [Required]
+        [MinLength(1)]
+        private string password;
+
+        [ObservableProperty]
+        [NotifyDataErrorInfo]
+        [Required]
+        [MinLength(1)]
+        private string repeatPassword;
+
+        public MainMenuViewModel()
+        {
+            IsLoggingFormVisibility = Visibility.Visible;
+            IsProfileCreationFormVisibility = Visibility.Collapsed;
+
+            CreateProfileButtonVisibility = Visibility.Visible;
+            LoginFormButtonVisibility = Visibility.Collapsed;
+        }
+
+        [RelayCommand]
+        public void CreateProfile()
+        {
+            Username = string.Empty;
+            Password = string.Empty;
+            RepeatPassword = string.Empty;
+
+            IsLoggingFormVisibility = Visibility.Collapsed;
+            IsProfileCreationFormVisibility = Visibility.Visible;
+
+            CreateProfileButtonVisibility = Visibility.Collapsed;
+            LoginFormButtonVisibility = Visibility.Visible;
+        }
+
+        [RelayCommand]
+        public void LoginForm()
+        {
+            Username = string.Empty;
+            Password = string.Empty;
+            RepeatPassword = string.Empty;
+
+            IsLoggingFormVisibility = Visibility.Visible;
+            IsProfileCreationFormVisibility = Visibility.Collapsed;
+
+            CreateProfileButtonVisibility = Visibility.Visible;
+            LoginFormButtonVisibility = Visibility.Collapsed;
+        }
+
+        [RelayCommand]
+        public void LoginProfileForm()
+        {
+        }
+
+        [RelayCommand]
+        public async Task CreateProfileFormAsync()
+        {
+            if (RepeatPassword != Password)
+            {
+                var metroWindow = (Application.Current.MainWindow as MetroWindow);
+                await metroWindow.ShowMessageAsync("Passwords are not equal!", null);
+                return;
+            }
+        }
+    }
+}
